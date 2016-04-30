9cb21b695273325575bd47b581f57382c735c1b2
sherylynn <352281674@qq.com>
2016-05-01 00:27:12 +0800

    热更新

--- android/app/src/main/java/com/meihu/MainActivity.java
+++ android/app/src/main/java/com/meihu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.meihu;
 
 import com.facebook.react.ReactActivity;
+import cn.reactnative.modules.update.UpdatePackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -13,6 +14,10 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+    @Override
+    protected String getJSBundleFile() {
+        return UpdateContext.getBundleUrl(this);
+    }
     @Override
     protected String getMainComponentName() {
         return "meihu";
@@ -34,7 +39,8 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new UpdatePackage()
         );
     }
 }
