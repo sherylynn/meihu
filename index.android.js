b70b523130ec8ed81c197282950315b2f509f0af
sherylynn <352281674@qq.com>
2016-05-03 22:16:32 +0800

    showdown test

--- index.android.js
+++ index.android.js
@@ -3,4 +3,7 @@
  * https://github.com/facebook/react-native
  */
 //import "./app-registry"; //meihu主体
-import "./test/pushy" //测试模块 react-native-pushy
+//import "./test/pushy" //测试模块 react-native-pushy
+//import "./test/showdown" //测试模块 react-native-showdown
+const test=["./app-registry","./test/pushy","./test/showdown" ,"./test/react-native-md-webview"]
+import "./test/showdown";
