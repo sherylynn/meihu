e4e801ff9bdc7e66928fb9ae48f4f71aad9e8e59
sherylynn <352281674@qq.com>
2016-05-09 14:30:14 +0800

    测试

--- index.android.js
+++ index.android.js
@@ -3,8 +3,9 @@
  * https://github.com/facebook/react-native
  */
 //const test=["./app-registry","./test/pushy","./test/showdown" ]
-import "./app-registry"; //meihu主体
+//import "./app-registry"; //meihu主体
 //import "./test/pushy" //测试模块 react-native-pushy
 //import "./test/showdown" //测试模块 react-native-showdown
 //import "./test/md-textinput";//test react-native-md-textinput
 //import "./test/lazyload";真不容易啊
+import "./test/pouchdb";
