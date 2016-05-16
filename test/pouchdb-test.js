3ae3687ee8e1eeb757eab14a70f188aca1931f08
sherylynn <352281674@qq.com>
2016-05-16 20:51:19 +0800

    sh5t

--- test/pouchdb-test.js
+++ test/pouchdb-test.js
@@ -4,12 +4,15 @@ import {AppRegistry, View, Text, TextInput, TouchableOpacity, Alert } from 'reac
 
 import PouchDB from 'pouchdb'
 import 'pouchdb-asyncstorage-down'
+//PouchDB.plugin(require('pouchdb-authentication'));
 //import './pouchdb.authentication.min.js'//死活注册不了这个事件 这个方法是浏览器的,强上不现实
-//PouchDB.plugin(require('./pouchdb.authentication.min.js'));//这种方法引入也是不行的
+PouchDB.plugin(require('./pouchdb.authentication.min.js'));//这种方法引入也是不行的
 //import Auth from 'pouchdb-auth'//依赖太多了,根本不可能实现
 //PouchDB.plugin(Auth)
 //const db = new PouchDB('mydb', { adapter: 'asyncstorage' })
-const db = new PouchDB('http://localhost:3000/db/shit');
+const db = new PouchDB('http://mh.kenx.cn:3000/db/shit');
+//const db = new PouchDB('http://192.168.0.249:5984/mydb', { skipSetup: true });
+//const db = new PouchDB('http://192.168.0.249:5984/mydb')
 //db.sync(remotedb);
 class Example extends Component {
     constructor() {
@@ -28,7 +31,7 @@ class Example extends Component {
         )
     };
     fuck = () => {
-        fuckdb.signup('batman', 'brucewayne', function (err, response) {
+        db.signup('batman', 'brucewayne', function (err, response) {
             if (err) {
                 if (err.name === 'conflict') {
                     // "batman" already exists, choose another username
@@ -39,7 +42,7 @@ class Example extends Component {
                 }
             } else {
                 Alert.alert('提示', '您的应用版本已更新,请前往应用商店下载新的版本', [
-                    { text: '确定', onPress: () => {} },
+                    { text: '确定', onPress: () => { } },
                 ]);
             }
         });
