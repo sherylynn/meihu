d0e7472a678e159000052c371b3ad5f37b14a75e
sherylynn <352281674@qq.com>
2016-06-13 16:17:06 +0800

    批处理

---
+++ start.bat
@@ -0,0 +1,4 @@
+start cmd /k node node_modules/react-native/local-cli/cli.js start
+start cmd /k "cd server && npm start"
+start cmd /k "cd .. && pouchdb-server -p 3456"
+start cmd /k emulator -avd meihu
