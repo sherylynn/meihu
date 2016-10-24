5797b8bd39300ffb73ab46fe64b063cd3b3c3051
sherylynn <352281674@qq.com>
2016-10-24 16:06:38 +0800

    start /min

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 start cmd /k node node_modules/react-native/local-cli/cli.js start
 start cmd /k "cd server && npm start"
 start cmd /k "cd .. && pouchdb-server -p 3456"
-start cmd /k emulator -avd meihu
-start cmd /k code ../meihu
+start /min cmd /k emulator -avd meihu
+start /min cmd /k code ../meihu
