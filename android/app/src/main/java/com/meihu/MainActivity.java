a83230e3076075499840b16553ace5377dde40da
sherylynn <352281674@qq.com>
2018-02-27 15:16:47 +0800

    更新了文件到0.54，但是傻逼的react-native-update的ndk又给我抛出了问题，还是不能运行

--- android/app/src/main/java/com/meihu/MainActivity.java
+++ android/app/src/main/java/com/meihu/MainActivity.java
@@ -17,10 +17,7 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getJSBundleFile() {
-        return UpdateContext.getBundleUrl(this);
-    }
+
     @Override
     protected String getMainComponentName() {
         return "meihu";
@@ -30,21 +27,11 @@ public class MainActivity extends ReactActivity {
      * Returns whether dev mode should be enabled.
      * This enables e.g. the dev menu.
      */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
+
 
     /**
      * A list of packages used by the app. If the app uses additional views
      * or modules besides the default ones, add more packages here.
      */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new RNDeviceInfo(),
-            new MainReactPackage(),
-            new UpdatePackage()
-        );
-    }
+
 }
--- android/app/src/main/java/com/meihu/MainApplication.java
+++ android/app/src/main/java/com/meihu/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import cn.reactnative.modules.update.UpdatePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,8 +25,14 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new UpdatePackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
